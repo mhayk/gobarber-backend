043b47f94df576997177a3e909a565ce799e3310
Mhayk Whandson <hi@mhayk.com>
2020-04-19 12:23:44 +0100

    The resource '/users' to create a new user has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+export default class CreateUserService {
+  public async execute({ name, email, password }): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email address already used!');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
