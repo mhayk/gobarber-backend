7249fd3cddb8671e40a50135d7f0e069083c7ac6
Mhayk Whandson <hi@mhayk.com>
2020-04-19 11:28:44 +0100

    The columns created_at and update_at have been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/database/migrations/1587200318921-CreateAppointments.ts
+++ src/database/migrations/1587200318921-CreateAppointments.ts
@@ -24,12 +24,22 @@ export default class CreateAppointments1587200318921
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<any> {
+  public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable('appointments');
   }
 }
