de5016aa8cd22cabff019b12f13b80dab4870bc8
Mhayk Whandson <hi@mhayk.com>
2020-09-25 16:07:36 +0100

    The dependecy injection has been configured.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
@@ -13,9 +14,12 @@ interface IRequest {
 /**
  * Dependency Inversion(SOLID)
  */
-
+@injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
   public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
