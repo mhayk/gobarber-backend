d98d1817924bfccc9d720bc5e095d629dd600dfe
Mhayk Whandson <hi@mhayk.com>
2020-10-11 17:32:17 +0100

    The unit test to verify that it should not be able to create a new user with a email that it is already being used.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
@@ -15,5 +17,24 @@ describe('CreateUser', () => {
     expect(user).toHaveProperty('id');
     expect(user).toHaveProperty('name');
     expect(user).toHaveProperty('password');
+  });
+
+  it('it should not be able to create a new user with email from another', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'Mhayk Whandson',
+      email: 'hi@mhayk.com',
+      password: '123',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Mhayk Whandson',
+        email: 'hi@mhayk.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
