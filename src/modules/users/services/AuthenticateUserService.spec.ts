a85ef79a170e34da248d699e2288992f6cf8bb3e
Mhayk Whandson <hi@mhayk.com>
2020-11-10 23:51:49 +0000

    A one more evaluation has been added into the AuthenticateUserService tests.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -18,7 +18,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'jhondoe@example.com',
       password: '1234',
@@ -30,5 +30,6 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
