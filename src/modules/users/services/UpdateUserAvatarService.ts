519722b577d0e167b3f951663e24aa9422a61dc9
Mhayk Whandson <hi@mhayk.com>
2020-11-12 23:27:49 +0000

    The UpdateUserAvatarService has been refactored to use the IStorageProvider.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,9 +1,7 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -13,10 +11,13 @@ interface IRequest {
 }
 
 @injectable()
-class UpdateUserAvatarSerive {
+class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -27,15 +28,12 @@ class UpdateUserAvatarSerive {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
@@ -43,4 +41,4 @@ class UpdateUserAvatarSerive {
   }
 }
 
-export default UpdateUserAvatarSerive;
+export default UpdateUserAvatarService;
