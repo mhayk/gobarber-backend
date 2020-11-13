22d6cf05b6a87e04b2d245db1e421213a4fdbda0
Mhayk Whandson <hi@mhayk.com>
2020-11-13 23:00:53 +0000

    test UpdateUserAvatartService: a test to check if the avatar is being deleted when a new one is informed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -44,4 +44,35 @@ describe('UpdateUserAvatar', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar-new.jpg',
+    });
+
+    expect(deleteFile).toBeCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar-new.jpg');
+  });
 });
