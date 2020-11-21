aea5cd2b0c21a5d74dc6bfe88db76a2abec44fed
Mhayk Whandson <hi@mhayk.com>
2020-11-21 20:06:22 +0000

    The parameters passed to sendMail method has been adjusted.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
-// import User from '../infra/typeorm/entities/User';
-
 interface IRequest {
   email: string;
 }
@@ -33,10 +31,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Password recovery request received: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: `Password recovery request received: ${token}`,
+      templateData: {
+        template: 'Hello, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
