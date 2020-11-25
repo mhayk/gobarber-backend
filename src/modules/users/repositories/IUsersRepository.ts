4a73d391ac2222398368735b166c1d9e86a85da8
Mhayk Whandson <hi@mhayk.com>
2020-11-25 22:58:06 +0000

    A IUsersRepository interface has been created and a new service ListProvidersService.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
+import IFindAllProvidersDTO from '@modules/appointments/dtos/IFindAllProvidersDTO';
 import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
+  findAllProviders(data?: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
