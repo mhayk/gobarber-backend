b6cdcc72f3da15f8daef92c24e68d77864270094
Mhayk Whandson <hi@mhayk.com>
2020-11-28 22:42:40 +0000

    The unit test from ListProviderDayAvailabilityService has been improved.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,12 +16,16 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 3, 18, 8, 0, 0),
+      date: new Date(2020, 3, 18, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 3, 18, 10, 0, 0),
+      date: new Date(2020, 3, 18, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 3, 18, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -34,9 +38,15 @@ describe('ListProviderMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
       ]),
     );
   });
