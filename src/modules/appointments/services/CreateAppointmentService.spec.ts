e1e605293eef08067d8bb7fe4e0135e8a3248a82
Mhayk Whandson <hi@mhayk.com>
2020-11-29 23:39:04 +0000

    A new test to check that the user cannot create an appointment on a past date.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -14,6 +14,9 @@ describe('CreateAppointment', () => {
     );
   });
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123',
@@ -25,7 +28,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointment on the same time', async () => {
-    const appointmentDate = new Date();
+    const appointmentDate = new Date(2026, 4, 10, 11);
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -33,9 +36,23 @@ describe('CreateAppointment', () => {
       user_id: '123',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
+        provider_id: '123',
+        user_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 11),
         provider_id: '123',
         user_id: '123',
       }),
