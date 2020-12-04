b4e045eb6636f2363e77ddf2b5406584c0b383db
Mhayk Whandson <hi@mhayk.com>
2020-12-04 19:59:45 +0000

    The service and their unit test to list the provider's appointments.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,43 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+/**
+ * [ { day: 1, available: false } ]
+ */
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
