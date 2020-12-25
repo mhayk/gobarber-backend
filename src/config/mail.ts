e430f6618a5f95ed43f038eecfdaef2a229a969a
Mhayk Whandson <hi@mhayk.com>
2020-12-25 08:44:00 +0000

    A default email config has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,6 +1,20 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'hi@mhayk.com',
+      name: 'Mhayk Whandson',
+    },
+  },
 } as IMailConfig;
